2ef394b95ac7122ff86b75aba290b2d097d8a1c9
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-03-03 14:04:49 -0500

    Styled Header compoennet

--- components/Page.jsx
+++ components/Page.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { element } from 'prop-types';
+
 import Header from './Header';
+import Meta from './Meta';
 
 const Page = ({ children }) => (
   <div>
+    <Meta />
     <Header />
     {children}
   </div>
